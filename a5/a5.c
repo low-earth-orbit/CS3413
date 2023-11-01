307deef20f938ef9d44971f0dc8257d4b1d920f5
low-earth-orbit <5917188+low-earth-orbit@users.noreply.github.com>
2023-10-31 21:51:01 -0300

    a5: use mutex

--- a5/a5.c
+++ a5/a5.c
@@ -27,6 +27,7 @@ typedef struct
     Job **queue;
     Job *summary;
     int quantum;
+    int id;
 } CpuThreadParam;
 
 // Printing thread parameter
@@ -34,6 +35,20 @@ typedef struct
 {
     Job *queue;
 } PrintingThreadParam;
+
+// Mutex for input queue
+pthread_mutex_t queue_mutex;
+
+typedef struct _buffer
+{
+    Job *job;
+    int cpu_id;
+    int time;
+} BufferEntry;
+
+BufferEntry printBuffer[1000];
+int buffer_index = 0;
+pthread_mutex_t buffer_mutex;
 
 void enqueue(Job **queue, Job *job)
 {
@@ -116,6 +131,23 @@ void updateSummary(Job *queue, char *userName, int time)
     }
 }
 
+void *printJobs(void *param)
+{
+    int local_index = 0;
+    while (1)
+    { // or a condition to exit
+        if (local_index < buffer_index)
+        {
+            pthread_mutex_lock(&buffer_mutex);
+            BufferEntry entry = printBuffer[local_index];
+            local_index++;
+            pthread_mutex_unlock(&buffer_mutex);
+
+            printf("Time: %d, CPU: %d, Job: %c\n", entry.time, entry.cpu_id, entry.job->processName);
+        }
+    }
+}
+
 void *simulate(void *param)
 {
     CpuThreadParam *p = (CpuThreadParam *)param;
@@ -124,6 +156,9 @@ void *simulate(void *param)
     Job **queue = p->queue;
     Job *summary = p->summary;
     int quantum = p->quantum;
+    int id = p->id;
+
+    printf("CPU thread: time = %d\t quantum = %d\t id = %d\n", cpu->time, quantum, id);
 
     int simulationStarted = 0;
     Job **runningQueue = (Job **)malloc(sizeof(Job *));
@@ -137,11 +172,24 @@ void *simulate(void *param)
     printf("Time Job\n");
     while (*runningQueue != NULL || *queue != NULL)
     {
+        pthread_mutex_lock(&queue_mutex);
+
         while (*queue != NULL && cpu->time >= (*queue)->arrivalTime)
         {
-            enqueue(runningQueue, *queue);
-            removeJob(queue, *queue);
+            // Add the job to local running queue if affinity matches CPU id
+            if ((*queue)->affinity == id)
+            {
+                enqueue(runningQueue, *queue);
+                removeJob(queue, *queue);
+            }
+            else // otherwise, move to the next one
+            {
+                queue = &((*queue)->next);
+            }
         }
+
+        pthread_mutex_unlock(&queue_mutex);
+
         if (cpu->current == NULL)
         {
             if (*runningQueue != NULL && cpu->time >= (*runningQueue)->arrivalTime)
@@ -157,6 +205,15 @@ void *simulate(void *param)
         {
             printf("%d\t%c\n", cpu->time, cpu->current->processName);
             cpu->current->duration--;
+
+            // Place job to print buffer after it's done processing
+            pthread_mutex_lock(&buffer_mutex);
+            printBuffer[buffer_index].job = cpu->current;
+            printBuffer[buffer_index].cpu_id = id;
+            printBuffer[buffer_index].time = cpu->time;
+            buffer_index++;
+            pthread_mutex_unlock(&buffer_mutex);
+
             if (cpu->current->duration == 0)
             {
                 updateSummary(summary, cpu->current->userName, cpu->time);
@@ -182,8 +239,10 @@ void *simulate(void *param)
         cpu->time++;
     }
     printf("%d\t-\n", cpu->time);
+
     pthread_exit(0);
 }
+
 void *printSummary(void *param)
 {
     PrintingThreadParam *p = (PrintingThreadParam *)param;
@@ -206,16 +265,16 @@ int main(int argc, char **argv)
     int quantums[numCpu]; // Create an array to store quantum for CPU
 
     /* For debug - Start */
-    for (int i = 0; i < numCpu; i++)
-    {
-        scanf("%d", &quantums[i]);
-    }
+    // for (int i = 0; i < numCpu; i++)
+    // {
+    //     scanf("%d", &quantums[i]);
+    // }
 
-    printf("numCpu = %d\n", numCpu);
-    for (int i = 0; i < numCpu; i++)
-    {
-        printf("quantums[%d] = %d\n", i, quantums[i]);
-    }
+    // printf("numCpu = %d\n", numCpu);
+    // for (int i = 0; i < numCpu; i++)
+    // {
+    //     printf("quantums[%d] = %d\n", i, quantums[i]);
+    // }
     /* For debug - End */
 
     Job *summary = NULL;
@@ -244,7 +303,7 @@ int main(int argc, char **argv)
     scanf("%s %s %s %s %s\n", buf, buf2, buf3, buf4, buf5); // Consume the title line
     while (EOF != scanf("%s %c %d %d %d", userName, &processName, &arrival, &duration, &affinity))
     {
-        printf("%s\t%c\t%d\t%d\t%d\n", userName, processName, arrival, duration, affinity);
+        // printf("%s\t%c\t%d\t%d\t%d\n", userName, processName, arrival, duration, affinity);
         if (contains(summary, userName) == 0)
         {
             newJob = (Job *)malloc(sizeof(Job));
@@ -271,12 +330,33 @@ int main(int argc, char **argv)
     }
 
     /* For debug - Start */
-    while (queue != NULL)
-    {
-        printf("%s\t%c\t%d\t%d\t%d\n", queue->userName, queue->processName, queue->arrivalTime, queue->duration, queue->affinity);
-        queue = queue->next;
-    }
+    // while (queue != NULL)
+    // {
+    //     printf("%s\t%c\t%d\t%d\t%d\n", queue->userName, queue->processName, queue->arrivalTime, queue->duration, queue->affinity);
+    //     queue = queue->next;
+    // }
     /* For debug - End */
+
+    if (pthread_mutex_init(&buffer_mutex, NULL) != 0)
+    {
+        perror("Failed to initialize buffer_mutex");
+        return 1;
+    }
+
+    // Create prining thread before creating CPU threads
+    pthread_t printing_thread;
+    if (pthread_create(&printing_thread, NULL, printJobs, NULL) != 0)
+    {
+        perror("Failed to create printing thread");
+        return 1;
+    }
+
+    // init mutext for input queue
+    if (pthread_mutex_init(&queue_mutex, NULL) != 0)
+    {
+        perror("Failed to initialize queue_mutex");
+        return 1; // or handle the error as appropriate
+    }
 
     // Create CPU threads
     pthread_t cpu_threads[numCpu];
@@ -288,6 +368,7 @@ int main(int argc, char **argv)
         cpu_thread_param[i].quantum = quantums[i];
         cpu_thread_param[i].queue = &queue;
         cpu_thread_param[i].summary = summary;
+        cpu_thread_param[i].id = i;
 
         if (pthread_create(&cpu_threads[i], NULL, simulate, &cpu_thread_param[i]) != 0)
         {
@@ -302,18 +383,11 @@ int main(int argc, char **argv)
         pthread_join(cpu_threads[i], NULL);
     }
 
-    pthread_t printing_thread;
-    PrintingThreadParam priting_thread_param;
-    // Define printing_thread_param
-    priting_thread_param.queue = summary;
-    if (pthread_create(&printing_thread, NULL, printSummary, &priting_thread_param) != 0)
-    {
-        perror("Failed to create printing thread\n");
-        return 1;
-    }
-
     // Join printing thread
     pthread_join(printing_thread, NULL);
+
+    pthread_mutex_destroy(&queue_mutex);
+    pthread_mutex_destroy(&buffer_mutex);
 
     return 0;
 }
