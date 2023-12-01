a307070f61c52ad2c2f87d27fe0abbd0a6514aab
low-earth-orbit <5917188+low-earth-orbit@users.noreply.github.com>
2023-12-01 17:08:35 -0400

    a9: complete read loop

--- a9/a9.c
+++ a9/a9.c
@@ -39,52 +39,60 @@ int main(int argc, char **argv)
     if (isPng(file))
     {
         printf("It's a PNG file\n");
+
+        while (!isTraverseFinished)
+        {
+            uint32_t lengthOfChunk;
+            char chunkType[5]; // 4 characters + null terminator
+
+            // Read the length of the chunk. 4 bytes. big-endian
+            if (fread(&lengthOfChunk, 1, 4, file) != 4)
+            {
+                perror("Unable to read file");
+                return EXIT_FAILURE;
+            }
+
+            // Convert to little-endian using endianness converter
+            lengthOfChunk = ntohl(lengthOfChunk);
+
+            // Read the chunk type (4 bytes)
+            if (fread(chunkType, 1, 4, file) != 4)
+            {
+                perror("Unable to read file");
+                return EXIT_FAILURE;
+            }
+
+            // Add null-terminator
+            chunkType[4] = '\0';
+
+            // Output chunk type and size
+            if (strcmp(chunkType, "IEND") == 0)
+            {
+                printf("Found %s\n", chunkType);
+                isTraverseFinished = true; // When the final chunk is encountered, the traversal finishes
+            }
+            else if (strcmp(chunkType, "IDAT") == 0)
+            {
+                printf("Found %s\n", chunkType);
+            }
+            else
+            {
+                printf("Found unknown: %s\n", chunkType);
+            }
+
+            printf("Chunk size is:%d\n", lengthOfChunk);
+
+            // Skip bytes
+            fseek(file, lengthOfChunk, SEEK_CUR);
+
+            // Skip CRC (4 bytes)
+            fseek(file, 4, SEEK_CUR);
+        }
     }
     else
     {
         printf("It's not a PNG file\n");
     }
-
-    if (!isTraverseFinished)
-    {
-
-        uint32_t lengthOfChunk;
-        char chunkType[5]; // 4 characters + null terminator
-
-        // Read the length of the chunk. 4 bytes. big-endian
-        if (fread(&lengthOfChunk, 1, 4, file) != 4)
-        {
-            perror("Unable to read file");
-            return EXIT_FAILURE;
-        }
-
-        // Convert to little-endian using endianness converter
-        lengthOfChunk = ntohl(lengthOfChunk);
-
-        // Read the chunk type (4 bytes)
-        if (fread(chunkType, 1, 4, file) != 4)
-        {
-            perror("Unable to read file");
-            return EXIT_FAILURE;
-        }
-
-        // Add null-terminator
-        chunkType[4] = '\0';
-
-        // Output chunk type and size
-        printf("Found unknown: %s\n", chunkType);
-        printf("Chunk size is:%d\n", lengthOfChunk);
-
-        // Skip bytes
-        fseek(file, lengthOfChunk, SEEK_CUR);
-
-        // Skip CRC (4 bytes)
-        fseek(file, 4, SEEK_CUR);
-
-        // To next chunk
-        // When the final chunk is encountered, the traversal finishes
-    }
-
     fclose(file);
 
     return EXIT_SUCCESS;
